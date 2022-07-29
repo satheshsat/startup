ed18e33e1db7e1507e7edf975afff17c86b0831d
Sathesh kumar <satheshs.sat@gmail.com>
2022-07-29 06:23:27 +0000

    created tab navigation

---
+++ src/screens/TabA.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { Card } from "@rneui/themed";
+import { NavigationContainerProps } from '@react-navigation/native';
+import { connect } from '../data/connect';
+
+interface OwnProps extends NavigationContainerProps {
+    navigation: any
+}
+
+interface StateProps {
+}
+
+interface DispatchProps {
+    
+}
+
+interface TabAProps extends OwnProps, StateProps, DispatchProps { }
+
+const TabA: React.FC<TabAProps> = ({ navigation }) => {
+
+    return (
+        <View style={styles.container}>
+            <Card>
+                <Text>TabA</Text>
+            </Card>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 200
+    },
+    cardcontent:{
+        
+    },
+    image: {
+        width: 50, 
+        height: 50
+    }
+  });
+
+  export default connect<OwnProps, StateProps, DispatchProps>({
+    mapStateToProps: (state) => ({
+        
+    }),
+    mapDispatchToProps: {
+      
+    },
+    component: TabA
+  })
